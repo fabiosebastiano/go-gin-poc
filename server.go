cdf0b242364d1cab3036fed1404594513458ec6e
Fabio Sebastiano <sebastiano.fabio@gmail.com>
2020-10-11 11:37:32 +0200

    implementati TEMPLATE HTML e GROUP SERVER

--- server.go
+++ server.go
@@ -24,23 +24,36 @@ func setupLogOutput() {
 func main() {
 	setupLogOutput()
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger(),
-		middlewares.BasicAuth(),
+
+	server.Static("/css", "./templates/css")
+	server.LoadHTMLGlob("templates/*.html")
+
+	server.Use(gin.Recovery(), middlewares.Logger()) //	middlewares.BasicAuth(),
 	//	, gindump.Dump()
-	)
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.FindAll())
-	})
+	//CREIAMO GRUPPO DI ROUTES PER LE API
+	apiRoutes := server.Group("api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
 
-	server.POST("/videos", func(ctx *gin.Context) {
-		err := videoController.Save(ctx)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid"})
-		}
-	})
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video input is valid"})
+			}
+		})
+	}
+
+	//ALTRO GRUPPO DI ROUTES CON LE VISTE/HTML TEMPLATES
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+		//	viewRoutes.POST("/videos", nil)
+	}
 
 	server.Run(":8080")
 }
